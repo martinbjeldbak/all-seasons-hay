05ce66babd094b05054f0c3fa8c6ee8d72f12fdd
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-07-14 23:12:40 -0700

    Remember to push in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # https://blog.docker.com/2014/08/orchestrating-docker-containers-in-production-using-fig/
-
+git push
 ssh mbm -t "cd /home/martin/all-seasons-hay; git pull ; sudo docker-compose build web && sudo docker-compose -f production.yml up -d --no-deps web"
