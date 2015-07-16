4fa51f553ad18d94f5514e6694b11a642e4297d2
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-07-16 13:32:02 -0700

    Make navbar minimize on scroll

--- app/public/javascripts/controllers.js
+++ app/public/javascripts/controllers.js
@@ -1,5 +1,24 @@
-var ctrls = angular.module('Ctrls', []);
+var ctrls = angular.module("Ctrls", []);
 
-ctrls.controller('MainController', function($scope) {
-  $scope.tagline = 'To the moon and back!';   
+ctrls.directive("scroll", function($window) {
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() {
+      if(this.pageYOffset >= 50) {
+        scope.minimizeNav = true;
+      }
+      else {
+        scope.minimizeNav = false;
+      }
+      scope.$apply();
+    });
+  };
+});
+
+ctrls.controller("MainController", function($scope) {
+  $scope.tagline = "To the moon and back!";
+  $scope.appName = "All Seasons Hay";
+});
+
+ctrls.controller("NavigationController", function($scope) {
+  $scope.services = "Services";
 });
