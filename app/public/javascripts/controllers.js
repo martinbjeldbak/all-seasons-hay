d1418d52a011a8671c085578c46b9042d1e2b245
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-07-16 15:28:22 -0700

    Work on header and footer

--- app/public/javascripts/controllers.js
+++ app/public/javascripts/controllers.js
@@ -10,12 +10,29 @@ ctrls.directive("scroll", function($window) {
 });
 
 ctrls.controller("MainController", function($scope) {
-  $scope.tagline = "To the moon and back!";
-  $scope.appName = "All Seasons Hay";
+
+  $scope.tagline = "Family owned and operated since 1982.";
+  $scope.appName = "All Seasons Hay Co.";
   $scope.products = "products";
   $scope.about = "about us";
   $scope.photos = "photos";
   $scope.contact = "contact us";
+  $scope.email = "allseasonshay@gmail.com";
+  $scope.phone1 = "909.795.2921";
+  $scope.phone2 = "909.795.2245";
+  $scope.fax = "909.795.2252";
+
+  $scope.addressStreet = "621 West Avenue L";
+  $scope.addressCity = "Calimesa";
+  $scope.addressState = "CA";
+  $scope.addressZip = "92320";
+
+  $scope.addressGmaps = "https://www.google.com/maps/place/621+E+Avenue+L,+Calimesa,+CA+92320/@33.999369,-117.020488,704m/data=!3m2!1e3!4b1!4m2!3m1!1s0x80db5079fd7a457f:0x14d12cd2a82a8d02";
+
+  $scope.cleanPhoneNr = function(phoneNr) {
+    return phoneNr.replace(/\./g, '');
+  };
+
 
   var services = angular.element(document.getElementById('services'));
 
