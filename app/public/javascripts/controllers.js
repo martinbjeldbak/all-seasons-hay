5a192c41b34b930683b111e846eb8456f55b5998
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-07-16 17:19:00 -0700

    Fix padding problems in navbar

--- app/public/javascripts/controllers.js
+++ app/public/javascripts/controllers.js
@@ -3,7 +3,7 @@ var ctrls = angular.module("Ctrls", ["duScroll"]);
 ctrls.directive("scroll", function($window) {
   return function(scope, element, attrs) {
     angular.element($window).bind("scroll", function() {
-      scope.minimizeNav = this.pageYOffset >= 50;
+      scope.minimizeNav = this.pageYOffset >= 100;
       scope.$apply();
     });
   };
